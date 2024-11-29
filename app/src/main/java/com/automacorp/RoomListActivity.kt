e6ca84475bfeb826c2cead1ef9f01fcb6718702b
Maelwen THOMAS <thomas.maelwen@gmail.com>
2024-11-29 15:02:02 +0100

    Updated how rooms are listed in list activity

--- app/src/main/java/com/automacorp/RoomListActivity.kt
+++ app/src/main/java/com/automacorp/RoomListActivity.kt
@@ -5,21 +5,35 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.automacorp.model.RoomDto
 import com.automacorp.service.RoomService
 import com.automacorp.ui.theme.AutomacorpTheme
+import com.automacorp.ui.theme.PurpleGrey80
+import androidx.compose.foundation.lazy.items
+
 
 class RoomListActivity : ComponentActivity() {
     companion object {
@@ -46,7 +60,6 @@ class RoomListActivity : ComponentActivity() {
                     ) },
                     modifier = Modifier.fillMaxSize()) { innerPadding ->
                     RoomList(
-                        rooms = RoomService.ROOMS, // Utilise les données de RoomService
                         modifier = Modifier.padding(innerPadding),
                         onRoomClick = onRoomClick
                     )
@@ -57,22 +70,56 @@ class RoomListActivity : ComponentActivity() {
 }
 
 @Composable
-fun RoomList(rooms: List<RoomDto>, modifier: Modifier = Modifier, onRoomClick: (RoomDto) -> Unit) {
-    LazyColumn(modifier = modifier.padding(16.dp)) {
-        item {
-            rooms.forEach { room ->
+fun RoomItem(room: RoomDto, modifier: Modifier = Modifier) {
+    Card(colors = CardDefaults.cardColors(containerColor = Color.Transparent),
+        border = BorderStroke(1.dp, PurpleGrey80)
+    ) {
+        Row(
+            modifier = modifier.padding(20.dp),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Column {
                 Text(
                     text = room.name,
                     style = MaterialTheme.typography.bodyLarge,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .clickable { onRoomClick(room) }
-                        .padding(vertical = 8.dp)
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = "Target temperature : " + (room.targetTemperature?.toString() ?: "?") + "°",
+                    style = MaterialTheme.typography.bodySmall
                 )
             }
+            Text(
+                text = (room.currentTemperature?.toString() ?: "?") + "°",
+                style = MaterialTheme.typography.headlineLarge,
+                textAlign = TextAlign.Right,
+                modifier = Modifier.fillMaxSize()
+            )
         }
     }
 }
+
+
+@Composable
+fun RoomList(modifier: Modifier = Modifier, onRoomClick: (RoomDto) -> Unit) {
+    LazyColumn(contentPadding = PaddingValues(4.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = modifier.padding(16.dp)) {
+        val rooms = RoomService.findAll()
+        items(rooms, key = { it.id }) {
+            RoomItem(
+                room = it,
+                modifier = Modifier
+                        .fillMaxWidth()
+                        .clickable { onRoomClick(it) }
+                        .padding(vertical = 8.dp)
+            )
+        }
+    }
+}
+
+
+
 
 
 
